779b7cd73c48027bfda7f7f3d2156d3499c8f829
Dusan Jonjin <dusan.jonjin@gmail.com>
2021-07-03 17:41:12 +0200

    Added images, store and utils folders with files

---
+++ src/Utilities/paths&apisData.js
@@ -0,0 +1,25 @@
+export const pathsAndApis = [
+    {   
+        path: '/top',
+        api: 'topstories'
+    }, 
+    {
+        path: '/new',
+        api: 'newstories'
+    }, 
+    {
+        path: '/comments'
+    },
+    {   
+        path: '/ask',
+        api: 'askstories'
+    }, 
+    {   
+        path: '/show',
+        api: 'showstories'
+    },  
+    {   
+        path: '/jobs',
+        api: 'jobstories'
+    }
+];
