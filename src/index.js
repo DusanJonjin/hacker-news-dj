03b6f728364b5ff78084f77a468ca2f8ca0b2417
Dusan Jonjin <dusan.jonjin@gmail.com>
2021-07-04 23:57:02 +0200

    Installed redux and react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HackerNewsApp from './HackerNewsApp';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducers } from './Store/reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
+
+const store = createStore(
+    reducers, /*preload state */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <React.StrictMode>
-        <HackerNewsApp />
+        <Router>
+            <Provider store={store}>
+                <HackerNewsApp />
+            </Provider> 
+        </Router>
     </React.StrictMode>,
      document.getElementById('root')
 );
