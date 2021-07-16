77e8ea493458bc4db3d5514adec3fff674b4c184
Dusan Jonjin <dusan.jonjin@gmail.com>
2021-07-17 00:26:48 +0200

    Initilized Header and Navbar components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import { Navbar } from './Navbar';
 
 export function Header() {
 
+    const [sideMenuOpen, setSideMenuOpen] = useState(false);
+
+    const handleSideMenuOpen = () => {
+        setSideMenuOpen(prevSideMenuOpen => !prevSideMenuOpen);
+    }
+
+    const closeSideMenu = () => {
+        setSideMenuOpen(false);
+    }
+
     return (
-        <header>
-            
+        <header className={`app-header`}>
+            <div id='logo-wrapper'>
+                <img src={require('../../Images/hn-logo.png').default}
+                     alt='hacker-news-custom-logo'
+                />
+                <h3>Hacker News App</h3>
+            </div>
+            <Navbar sideMenuOpen={sideMenuOpen}
+                    closeSideMenu={closeSideMenu}
+            />
+            <div className='theme-toggle-wrap' >
+                <div className={`theme-toggle-bar `}
+                >                 
+                </div>
+            </div>
+            <div className='hamburger-wrap' onClick={() => handleSideMenuOpen()}>
+                <div className={`hamburger ${sideMenuOpen ? 'menu-open' : ''}`}></div>
+            </div>
         </header>
     )
 }
--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,32 @@
-export default function Navbar() {
+import { pathsAndApis } from '../../Utilities/paths&apisData';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+export function Navbar({ sideMenuOpen, closeSideMenu }) {
+
+    const { pathname } = useLocation();
+
+    const navLinks = pathsAndApis.map((obj, i) => 
+        <li 
+            key={i}
+            className={`nav-li ${pathname.includes(obj.path) ? 'selected-link' : ''}`} 
+            onClick={() => closeSideMenu()}
+        >
+            <Link 
+                to={{pathname: obj.path}}
+                className='nav-link'
+            >
+                {obj.path.slice(1, obj.path.length)}
+            </Link>
+        </li>
+    );
 
     return (
-        <nav>
+        <nav 
+            className={`navbar ${sideMenuOpen ? 'nav-side-open' : ''}`}
+        >
             <ul>
-                links
+                {navLinks}
             </ul>
         </nav>
     )
