68e2d8981a27a876d1082e2b8c865c145d4ee64b
Dusan Jonjin <dusan.jonjin@gmail.com>
2021-08-27 01:10:52 +0200

    Warnings deployment bug solved

--- src/Components/Comments/ReferredStory.jsx
+++ src/Components/Comments/ReferredStory.jsx
@@ -1,14 +1,13 @@
 import React, {useState, useEffect}from 'react';
 import { Title } from '../- Shared -/Title';
 import { getItem } from '../../API/ApiCalls';
-import { themedClass } from '../../Utilities/helperFunctions';
 import { usePreventSetStateOnUnmount } from '../../Hooks/PreventSetStateOnUnmount';
 import { useSelector } from 'react-redux';
 import '../../Styles/Comments/RefferedStory.css'
 
 export function ReferredStory({ parent }) {
 
-    const { dark, modern } = useSelector(state => state.theme); 
+    const { dark } = useSelector(state => state.theme); 
 
     const [story, setStory] = useState({status: 'isloading', item: {}});
 
